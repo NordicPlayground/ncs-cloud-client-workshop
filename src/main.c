c56a8d23a91b51fcb1b553ae229380ec93a7be25
Torbjørn Øvrebekk <torbjorn.ovrebekk@nordicsemi.no>
2022-05-11 14:24:10 +0200

    BME680 sensor readout added as per the sample. Thread created in cloud_client to read the sensor at 3 second intervals

--- src/main.c
+++ src/main.c
@@ -306,3 +306,30 @@ void main(void)
 
 	k_work_schedule(&connect_work, K_NO_WAIT);
 }
+
+#include <drivers/sensor.h>
+
+void on_sensor_read(void)
+{
+	const struct device *dev = device_get_binding(DT_LABEL(DT_INST(0, bosch_bme680)));
+	struct sensor_value temp, press, humidity, gas_res;
+
+	printf("Device %p name is %s\n", dev, dev->name);
+
+	while (1) {
+		k_sleep(K_MSEC(3000));
+
+		sensor_sample_fetch(dev);
+		sensor_channel_get(dev, SENSOR_CHAN_AMBIENT_TEMP, &temp);
+		sensor_channel_get(dev, SENSOR_CHAN_PRESS, &press);
+		sensor_channel_get(dev, SENSOR_CHAN_HUMIDITY, &humidity);
+		sensor_channel_get(dev, SENSOR_CHAN_GAS_RES, &gas_res);
+
+		printf("T: %d.%06d; P: %d.%06d; H: %d.%06d; G: %d.%06d\n",
+				temp.val1, temp.val2, press.val1, press.val2,
+				humidity.val1, humidity.val2, gas_res.val1,
+				gas_res.val2);
+	}
+}
+
+K_THREAD_DEFINE(my_sensor_thread, 512, on_sensor_read, 0, 0, 0, 5, 0, 0);
