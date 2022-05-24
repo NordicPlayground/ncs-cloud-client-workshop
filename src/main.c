d529556dbbf41bb7252be6087dfe0b4b44d81600
Torbjørn Øvrebekk <torbjorn.ovrebekk@nordicsemi.no>
2022-05-24 11:11:34 +0200

    BME680 support added by copying code from the sample

--- src/main.c
+++ src/main.c
@@ -9,6 +9,7 @@
 #include <modem/lte_lc.h>
 #include <net/cloud.h>
 #include <net/socket.h>
+#include <drivers/sensor.h>
 #include <dk_buttons_and_leds.h>
 
 #include <logging/log.h>
@@ -278,6 +279,9 @@ void main(void)
 
 	LOG_INF("Cloud client has started");
 
+	const struct device *dev = device_get_binding(DT_LABEL(DT_INST(0, bosch_bme680)));
+	struct sensor_value temp, press, humidity, gas_res;
+
 	cloud_backend = cloud_get_binding(CONFIG_CLOUD_BACKEND);
 	__ASSERT(cloud_backend != NULL, "%s backend not found",
 		 CONFIG_CLOUD_BACKEND);
@@ -305,4 +309,19 @@ void main(void)
 	LOG_INF("Connecting to cloud");
 
 	k_work_schedule(&connect_work, K_NO_WAIT);
+
+	while (1) {
+		k_sleep(K_MSEC(3000));
+
+		sensor_sample_fetch(dev);
+		sensor_channel_get(dev, SENSOR_CHAN_AMBIENT_TEMP, &temp);
+		sensor_channel_get(dev, SENSOR_CHAN_PRESS, &press);
+		sensor_channel_get(dev, SENSOR_CHAN_HUMIDITY, &humidity);
+		sensor_channel_get(dev, SENSOR_CHAN_GAS_RES, &gas_res);
+
+		printf("T: %d.%06d; P: %d.%06d; H: %d.%06d; G: %d.%06d\n",
+				temp.val1, temp.val2, press.val1, press.val2,
+				humidity.val1, humidity.val2, gas_res.val1,
+				gas_res.val2);
+	}
 }
